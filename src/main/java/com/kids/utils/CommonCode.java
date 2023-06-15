2a2bc1f75d62dbaa5a29cb160eb14f8aac3a19ce
YWKIM <ywgim7777@gmail.com>
2023-06-15 13:48:14 +0900

    init project

---
+++ src/main/java/com/kids/utils/CommonCode.java
@@ -0,0 +1,10 @@
+package com.kids.utils;
+
+public interface CommonCode {
+	//프로그램 전체에서 참조할 코드표
+	
+	//User 관련 코드
+	String USER_USER_TYPE_CUSTOMER = "CUS";
+	String USER_USER_TYPE_ADMIN = "ADM";
+	 
+}
